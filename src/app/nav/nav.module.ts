70d83b3bd415e57f2db1954dbcc3eea5d2191e46
Kyle Stankovich <gamemakergml@gmail.com>
2020-11-19 19:06:57 -0500

    Added Video.js; Added common Angular Material elements.

--- src/app/nav/nav.module.ts
+++ src/app/nav/nav.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavComponent } from './nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { AppMatModule } from '../shared/app-mat.module';
 
 @NgModule({
   declarations: [
     NavComponent
   ],
   imports: [
+    AppMatModule,
     CommonModule,
     LayoutModule,
     RouterModule
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AppMatModule } from './app-mat.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    AppMatModule
+  ],
+  exports: [
+    AppMatModule
+  ]
+})
+export class SharedModule {}
